0d7b5dafcdcb504a7d06c361c28c56eff99e7aaf
rsa568422 <rsa568422@hotmail.com>
2018-11-05 21:21:10 +0100

    en la pagina de las aseguradoras se agrega una lista de logotipos de las empresas, con imagenes obtenidas del proveedor.

--- web/Controlador/ControladorAseguradoras.js
+++ web/Controlador/ControladorAseguradoras.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
-    var color = $("#btn-aseguradoras").css("background-color");
+    
+    function mostrarLogo(aseguradora) {
+        var i = "<img src='data:image/gif;base64," + aseguradora.logo + "' width='72' height='54' alt='" + aseguradora.nombre + "'/>";
+        $("#listaAseguradoras").append(i);
+        $("#listaAseguradoras").children("img").css("margin-right", "1%");
+    }
+        
+    var aseguradoras, color = $("#btn-aseguradoras").css("background-color");
     $("#ventana").css("border-color", color);
     $("#aseguradoras").css("border-color", color);
+    $("#listaAseguradoras").css("border-color", color);
     $("#peritos").css("border-color", color);
     $("#trabajos").css("border-color", color);
     color = color.substring(0, color.length - 1) + ", 0.3)";
     $("#ventana").css("background-color", color);
     
+    $.get("http://localhost:8080/ReForms_Provider/wr/aseguradora/obtenerAseguradoras", function(data, status) {
+        if (status == "success") {
+            aseguradoras = data;
+            var i;
+            for (i = 0; i < aseguradoras.length; i++) {
+                mostrarLogo(aseguradoras[i]);
+            }
+        }
+    }, "json");
 });
