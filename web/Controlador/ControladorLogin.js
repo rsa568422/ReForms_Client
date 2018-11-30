5224ea01ddb051fba658f2c522bcb7f813b1d9a7
rsa568422 <rsa568422@hotmail.com>
2018-11-30 21:46:41 +0100

    mas cosas para los trabajadores

--- web/Controlador/ControladorLogin.js
+++ web/Controlador/ControladorLogin.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     
-    var operadores, googleKey = 'AIzaSyCGDBVMfMKCQcSbgMHxHH6zpOevYQzkYko';
+    var operadores, googleKey = '';
     
     function acceder() {
         $('#barra').load('Html/barra.html', function(responseTxt, statusTxt) {
